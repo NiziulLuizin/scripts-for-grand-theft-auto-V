76181bb8ea9bc141c485fa59a6ba94b224cafe6a
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-17 16:38:41 -0300

    I removed some properties and methods, and other little things that were temporarily in main. I ended up creating the text manager right away, with some methods and events taken from the main one.

--- Speedometer/Speedometer-for-bicycle/Main.cs
+++ Speedometer/Speedometer-for-bicycle/Main.cs
@@ -1,108 +1,31 @@
 ﻿using Speedometer_for_bicycle.Draw.Sprites;
-using System.Windows.Forms;
-using System.IO;
 using GTA.UI;
-using System;
 using GTA;
-using Speedometer_for_bicycle.Draw.Settings.For_The_Text_Elements;
 
 namespace Speedometer_for_bicycle
 {
     public class Main : Script
     {
-        float PtfX = 0f;
-        float PtfY = 0f;
-
-        float SzX = 0f;
-        float SzY = 0f;
-
-        float Velocity;
         public Main()
         {
             NotifyWhenTheScriptHasBeenLoaded();         
 
             Tick += (o, e) =>
             {
-                //drawInfos();
-
                 if (Game.Player.Character.IsSittingInVehicle())
                 {
                     if(Game.Player.Character.CurrentVehicle.Type == VehicleType.Bicycle)
                     {
-                        Sprite_Manager.ReturnTheCurrentSpeedometer();
-                        //Distance_Traveled_Text_Element.Position = new System.Drawing.PointF(PtfX, PtfY);
-                    }
-                    else if (Game.Player.Character.CurrentVehicle.Type == VehicleType.Helicopter)
-                    {
-                        var Speed = new TextElement((Game.Player.Character.CurrentVehicle.Speed * 1.94384f).ToString("0"), new System.Drawing.PointF(644f, 662f), 0.50f, System.Drawing.Color.White, Font.Monospace, Alignment.Center);
-                        Speed.Draw();
+                        SpriteManager.ReturnTheCurrentSpeedometer();
+                        //Distance_Traveled_Text_Element.Position = new System.Drawing.PointF(TextManager.ptfX, TextManager.ptfY);
                     }
                 }     
-            };
-
-            KeyDown += (o, e) =>
-            {
-                if (e.KeyCode == Keys.Up)
-                    PtfY -= 1 + Velocity;
-                else if(e.KeyCode == Keys.Down)
-                    PtfY += 1 + Velocity;
-
-                if (e.KeyCode == Keys.Left)
-                    PtfX -= 1 + Velocity;
-                else if (e.KeyCode == Keys.Right)
-                    PtfX += 1 + Velocity;
-
-                if(e.KeyCode == Keys.D9)
-                    SzX = Convert.ToSingle(Game.GetUserInput());
-                else if(e.KeyCode == Keys.D8)
-                    SzY = Convert.ToSingle(Game.GetUserInput());
-
-
-                if (e.KeyCode == Keys.D7)
-                {
-                    var Input = Game.GetUserInput();
-
-                    if (Input == "SavePosition")
-                        SaveConfigOfPosition();
-                    else if (Input == "SaveSize")
-                        SaveConfigOfSize();
-                }
-
-                if (e.KeyCode == Keys.Oemplus)
-                    Velocity += 1;
-                else if (e.KeyCode == Keys.OemMinus)
-                    Velocity -= 1;
-            };
+            }; 
         }
 
         void NotifyWhenTheScriptHasBeenLoaded()
         {
-            GTA.UI.Screen.ShowHelpText("~r~Script loaded~w~!", 5000);
-        }
-
-        void SaveConfigOfPosition()
-        {
-            using (var ConfigPosition = new StreamWriter(GetRelativeFilePath("Image\\ConfigPosition.txt")))
-                ConfigPosition.WriteLine($"Position X: {PtfX} Position Y: {PtfY}");
-        }
-
-        void SaveConfigOfSize()
-        {
-            using (var ConfigPosition = new StreamWriter(GetRelativeFilePath("Image\\ConfigSize.txt")))
-                ConfigPosition.WriteLine($"Size X: {SzX} Size Y: {SzY}"); 
-        }
-
-        static string ReturnWhatYouReadInTheLegacy(string path)
-        {
-            using (var FileContent = new StreamReader(path))
-                return FileContent.ReadToEnd();
-        }
-
-
-        void drawInfos()
-        {
-            var InformationGroup = new TextElement($"Position X: ~r~{PtfX}~w~ Position Y: ~r~{PtfY}~w~ Velocity: ~r~{Velocity}~w~", new System.Drawing.PointF(200f, 200f), 0.40f);
-                InformationGroup.Draw();
+            Screen.ShowHelpText("~r~Script loaded~w~!", 5000);
         }
     }
 }
---
+++ Speedometer/Speedometer-for-bicycle/Text-Manager/TextManager.cs
@@ -0,0 +1,88 @@
+﻿using GTA;
+using GTA.UI;
+using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Speedometer_for_bicycle.Text_Manager
+{
+    internal class TextManager : Script
+    {
+        internal static float ptfX = 0f;
+        internal static float ptfY = 0f;
+
+        internal static float szX = 0f;
+        internal static float szY = 0f;
+
+        static float velocity;
+
+        private bool isEditorModeEnabled = false;
+        public TextManager()
+        {
+            Tick += (o, e) =>
+            {
+                if (isEditorModeEnabled)
+                    drawInfos();
+            };
+
+            KeyDown += (o, e) =>
+            {
+                if(isEditorModeEnabled)
+                {
+                    if (e.KeyCode == Keys.Up)
+                        ptfY -= 1 + velocity;
+                    else if (e.KeyCode == Keys.Down)
+                        ptfY += 1 + velocity;
+
+                    if (e.KeyCode == Keys.Left)
+                        ptfX -= 1 + velocity;
+                    else if (e.KeyCode == Keys.Right)
+                        ptfX += 1 + velocity;
+                }
+
+                if (e.KeyCode == Keys.OemQuotes)
+                {
+                    var Input = Game.GetUserInput();
+
+                    if (Input == "SavePosition")
+                        SaveConfigOfPosition();
+                    else if (Input == "SaveSize")
+                        SaveConfigOfSize();
+                    else if (Input == "EditModeOn")
+                        isEditorModeEnabled = true;
+                    else if (Input == "EditModeOff")
+                        isEditorModeEnabled = false;
+                }
+
+                if (e.KeyCode == Keys.Oemplus)
+                    velocity += 1;
+                else if (e.KeyCode == Keys.OemMinus)
+                    velocity -= 1;
+            };
+        }
+
+        void SaveConfigOfPosition()
+        {
+            using (var ConfigPosition = new StreamWriter(GetRelativeFilePath("Image\\ConfigPosition.txt")))
+                ConfigPosition.WriteLine($"Position X: {ptfX} Position Y: {ptfY}");
+        }
+
+        void SaveConfigOfSize()
+        {
+            using (var ConfigPosition = new StreamWriter(GetRelativeFilePath("Image\\ConfigSize.txt")))
+                ConfigPosition.WriteLine($"Size X: {szX} Size Y: {szY}");
+        }
+
+        static string ReturnWhatYouReadInTheLegacy(string path)
+        {
+            using (var FileContent = new StreamReader(path))
+                return FileContent.ReadToEnd();
+        }
+
+        void drawInfos()
+        {
+            var InformationGroup = new TextElement($"Position X: ~r~{ptfX}~w~ Position Y: ~r~{ptfY}~w~ Velocity: ~r~{velocity}~w~", new System.Drawing.PointF(200f, 200f), 0.40f);
+            InformationGroup.Draw();
+        }
+    }
+}
