0af94caacd8dbe8f12e654db5be5439e307befaf
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-19 17:57:46 -0300

    I removed some things that were temporarily in the text manager for the newly created "Editor Mode"

---
+++ Speedometer/Speedometer-for-bicycle/Editor-Mode/EditorMode.cs
@@ -0,0 +1,168 @@
+﻿using GTA;
+using GTA.UI;
+using System.Windows.Forms;
+using Speedometer.Draw.Text_Element.Editor_Mode.Informations.Sprites;
+using Speedometer.Draw.Text_Element.Editor_Mode.Informations.Text_Elements;
+
+namespace Speedometer.Editor_Mode
+{
+    internal class EditorMode : Script
+    {
+        private static bool _isEditorModeEnabled = false;
+        private static bool _isToEditThePosition = false;
+        private static bool _isToEditTheSize = false;
+
+        private static string _element;
+        private static float _velocity;
+
+        private static float _ptfX = 0f;
+        private static float _ptfY = 0f;
+        private static float _szX = 0f;
+        private static float _szY = 0f;
+
+
+        internal static bool IsEditorModeEnabled 
+        { get { return _isEditorModeEnabled; } }
+        internal static bool IsToEditThePosition 
+        { get { return _isToEditThePosition; } }
+        internal static bool IsToEditTheSize 
+        { get { return _isToEditTheSize; } }
+
+        internal static float Velocity
+        { get { return _velocity;  } }
+        internal static string Element
+        { get { return _element; } }
+
+        internal static float PtfX 
+        { get { return _ptfX; } }
+        internal static float PtfY 
+        { get { return _ptfY; } }
+        internal static float SzX 
+        { get { return _szX; } }
+        internal static float SzY 
+        { get { return _szY; } }
+
+
+
+        public EditorMode()
+        {
+            KeyDown += (o, e) =>
+            {
+                if (IsEditorModeEnabled)
+                {
+                    if (IsToEditThePosition)
+                    {
+                        if (e.KeyCode is Keys.Up)
+                            _ptfY -= 1 + _velocity;
+                        else if (e.KeyCode == Keys.Down)
+                            _ptfY += 1 + _velocity;
+
+                        if (e.KeyCode is Keys.Left)
+                            _ptfX -= 1 + _velocity;
+                        else if (e.KeyCode == Keys.Right)
+                            _ptfX += 1 + _velocity;
+                    }
+                    else if (IsToEditTheSize)
+                    {
+                        if (e.KeyCode is Keys.Up)
+                            _szY -= 1 + _velocity;
+                        else if (e.KeyCode == Keys.Down)
+                            _szY += 1 + _velocity;
+
+                        if (e.KeyCode is Keys.Left)
+                            _szX -= 1 + _velocity;
+                        else if (e.KeyCode == Keys.Right)
+                            _szX += 1 + _velocity;
+                    }
+                }
+
+                if (e.KeyCode is Keys.D1)
+                {
+                    var Input = Game.GetUserInput();
+
+                    switch (Input)
+                    {
+                        case "SavePosition()":
+                            //SaveConfigOfPosition();
+                            break;
+                        case "SaveSize()":
+                            //SaveConfigOfSize();
+                            break;
+                        case "Edit Mode: On":
+                            _isEditorModeEnabled = true;
+                            break;
+                        case "Edit Mode: Off":
+                            _isEditorModeEnabled = false;
+                            _isToEditThePosition = false;
+                            _isToEditTheSize = false;
+                            break;
+                        case "Text Element: Distance":
+                            _element = "Distance";
+                            break;
+                        case "Text Element: Speed":
+                            _element = "Speed";
+                            break;
+                        case "Text Element: Time":
+                            _element = "Time";
+                            break;
+                        case "Sprite: Speedometer":
+                            _element = "Speedometer";
+                            break;
+                    }
+                }
+
+                if (e.KeyCode is Keys.D2)
+                {
+                    if (IsEditorModeEnabled)
+                    {
+                        var Input = Game.GetUserInput();
+
+                        switch (Input)
+                        {
+                            case "Edit Position":
+                                _isToEditThePosition = true;
+                                _isToEditTheSize = false;
+                                break;
+                            case "Edit Size":
+                                _isToEditTheSize = true;
+                                _isToEditThePosition = false;
+                                break;
+                        }
+                    }
+                }
+
+                if (e.KeyCode is Keys.Oemplus)
+                    _velocity += 1;
+                else if (e.KeyCode is Keys.OemMinus)
+                    _velocity -= 1;
+            };
+        }
+
+        internal static void DrawInfos()
+        {
+            string instructions = "~b~Set what you will edit~w~";
+            string instructions2 = "~y~Use the D2 key to choose between~w~ '~y~Edit Position~w~' or '~y~Edit Size~w~'";
+
+            if (_element is null)
+            {
+                new TextElement($"Mode Edit: [~g~{IsEditorModeEnabled}~w~] / [{instructions}] [{instructions2}] (Element: ~o~{_element}~w~)", new System.Drawing.PointF(200f, 200f), 0.40f).Draw();
+            }
+
+            switch (_element)
+            {
+                case "Speedometer":
+                    new SpeedometerAttributes();
+                    break;
+                case "Distance":
+                    new DistanceAttributes();
+                    break;
+                case "Speed":
+                    new SpeedAttributes();
+                    break;
+                case "Time":
+                    new TimeAttributes();
+                    break;
+            }
+        }
+    }
+}
--- Speedometer/Speedometer-for-bicycle/Text-Manager/TextManager.cs
+++ Speedometer/Speedometer-for-bicycle/Text-Manager/TextManager.cs
@@ -1,178 +1,39 @@
-﻿using GTA;
-using GTA.UI;
-using System.IO;
-using System.Windows.Forms;
-using Speedometer.Draw.Settings.For_The_Sprites;
-using Speedometer.Draw.Text_Element.Editor_Mode.Informations.Sprites;
-using Speedometer.Draw.Text_Element.Editor_Mode.Informations.Text_Elements;
+﻿using System.IO;
 
 namespace Speedometer.Text_Manager
 {
-    internal class TextManager : Script
+    internal class TextManager
     {
-        internal static string element;
+        private static string DirectoryForTheSettings
+        { get { return new Main().GetRelativeFilePath("SpeedometerSettings"); } }
 
-        internal static float ptfX = 0f;
-        internal static float ptfY = 0f;
+        private static readonly string _directoryForSettingTheSpriteSize = $"{DirectoryForTheSettings}\\Sprites\\Size.txt";
+        private static readonly string _directoryForSettingTheSpritePosition = $"{DirectoryForTheSettings}\\Sprites\\Position.txt";
 
-        internal static float szX = 0f;
-        internal static float szY = 0f;
-
-        internal static float velocity;
-
-        private static bool _isEditorModeEnabled = false;
-        private static bool _isToEditThePosition = false;
-        private static bool _isToEditTheSize = false;
-        internal static bool IsEditorModeEnabled { get { return _isEditorModeEnabled; } }
-        internal static bool IsToEditThePosition { get { return _isToEditThePosition; } }
-        internal static bool IsToEditTheSize { get { return _isToEditTheSize; } }
+        private static readonly string _directoryForTheConfigurationOfThePositionOfScale = $"{DirectoryForTheSettings}\\Text-Element\\Scale.txt";
+        private static readonly string _directoryForTheConfigurationOfThePositionOfTheTextElement = $"{DirectoryForTheSettings}\\Text-Element\\Position.txt";
 
         public TextManager()
         {
-            Tick += (o, e) =>
-            {
-                if (IsEditorModeEnabled)
-                {
-                    DrawInfos();
-                    Game.DisableControlThisFrame(GTA.Control.Phone);
-                }
-            };
 
-            KeyDown += (o, e) =>
-            {
-                if (IsEditorModeEnabled)
-                {
-                    if (IsToEditThePosition)
-                    {
-                        if (e.KeyCode == Keys.Up)
-                            ptfY -= 1 + velocity;
-                        else if (e.KeyCode == Keys.Down)
-                            ptfY += 1 + velocity;
-
-                        if (e.KeyCode == Keys.Left)
-                            ptfX -= 1 + velocity;
-                        else if (e.KeyCode == Keys.Right)
-                            ptfX += 1 + velocity;
-                    }
-                    else if (IsToEditTheSize)
-                    {
-                        if (e.KeyCode == Keys.Up)
-                            szY -= 1 + velocity;
-                        else if (e.KeyCode == Keys.Down)
-                            szY += 1 + velocity;
-
-                        if (e.KeyCode == Keys.Left)
-                            szX -= 1 + velocity;
-                        else if (e.KeyCode == Keys.Right)
-                            szX += 1 + velocity;
-                    }
-                }
-
-                if (e.KeyCode == Keys.D1)
-                {
-                    var Input = Game.GetUserInput();
-
-                    switch (Input)
-                    {
-                        case "SavePosition()":
-                            SaveConfigOfPosition();
-                            break;
-                        case "SaveSize()":
-                            SaveConfigOfSize();
-                            break;
-                        case "Edit Mode: On":
-                            _isEditorModeEnabled = true;
-                            break;
-                        case "Edit Mode: Off":
-                            _isEditorModeEnabled = false;
-                            _isToEditThePosition = false;
-                            _isToEditTheSize = false;
-                            break;
-                        case "Text Element: Distance":
-                            element = "Distance";
-                            break;
-                        case "Text Element: Speed":
-                            element = "Speed";
-                            break;
-                        case "Text Element: Time":
-                            element = "Time";
-                            break;
-                        case "Sprite: Speedometer":
-                            element = "Speedometer";
-                            break;
-                    }
-                }
-
-                if (e.KeyCode == Keys.D2)
-                {
-                    if (IsEditorModeEnabled)
-                    {
-                        var Input = Game.GetUserInput();
-
-                        switch (Input)
-                        {
-                            case "Edit Position":
-                                _isToEditThePosition = true;
-                                _isToEditTheSize = false;
-                                break;
-                            case "Edit Size":
-                                _isToEditTheSize = true;
-                                _isToEditThePosition = false;
-                                break;
-                        }
-                    }
-                }
-
-                if (e.KeyCode == Keys.Oemplus)
-                    velocity += 1;
-                else if (e.KeyCode == Keys.OemMinus)
-                    velocity -= 1;
-            };
         }
 
         void SaveConfigOfPosition()
         {
-            using (var ConfigPosition = new StreamWriter(GetRelativeFilePath("Image\\ConfigPosition.txt")))
-                ConfigPosition.WriteLine($"Position X: {SpeedometerSettings.Position.X} Position Y: {SpeedometerSettings.Position.Y}");
+            //using (var Position = new SktreamWriter()
+                //Position.WriteLine();
         }
 
         void SaveConfigOfSize()
         {
-            using (var ConfigPosition = new StreamWriter(GetRelativeFilePath("Image\\ConfigSize.txt")))
-                ConfigPosition.WriteLine($"Size X: {SpeedometerSettings.Size.Width} Size Y: {SpeedometerSettings.Size.Height}");
+            //using (var Size = new StreamWriter()
+                //Size.WriteLine();
         }
 
         static string ReturnWhatYouReadInTheLegacy(string path)
         {
             using (var FileContent = new StreamReader(path))
                 return FileContent.ReadToEnd();
-        }
-
-        void DrawInfos()
-        {
-            string instructions = "~b~Set what you will edit~w~";
-            string instructions2 = "~y~Use the D2 key to choose between~w~ '~y~Edit Position~w~' or '~y~Edit Size~w~'";
-
-            if (element == null)
-            {
-                new TextElement($"Mode Edit: [~g~{IsEditorModeEnabled}~w~] / [{instructions}] [{instructions2}] (Element: ~o~{element}~w~)", new System.Drawing.PointF(200f, 200f), 0.40f).Draw();
-            }
-
-            switch (element)
-            {
-                case "Speedometer":
-                    new SpeedometerAttributes();
-                    break;
-                case "Distance":
-                    new DistanceAttributes();
-                    break;
-                case "Speed":
-                    new SpeedAttributes();
-                    break;
-                case "Time":
-                    new TimeAttributes();
-                    break;
-            }
         }
     }
 }
