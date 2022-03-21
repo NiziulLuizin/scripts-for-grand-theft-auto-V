270fe5c2ea99a822ad247941d83c47da82ecd236
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-20 23:39:34 -0300

    testing the logic for storing and querying parameters for drawings

--- Speedometer/Text-Manager/TextManager.cs
+++ Speedometer/Text-Manager/TextManager.cs
@@ -1,21 +1,20 @@
-﻿using System.IO;
+﻿using GTA;
+using Speedometer.Draw.Settings.For_The_Sprites;
+using Speedometer.Editor_Mode;
+using System.IO;
+using System.Windows.Forms;
 
 namespace Speedometer.Text_Manager
 {
-    internal class TextManager
+    internal class TextManager : Script
     {
-        private static string DirectoryForTheSettings
-        { get { return new Main().GetRelativeFilePath("SpeedometerSettings"); } }
+        private static readonly string _directoryForSettingTheSprite = $"\\Sprites\\Sprite-Parameter.ini";
 
-        private static readonly string _directoryForSettingTheSpriteSize = $"{DirectoryForTheSettings}\\Sprites\\Size.txt";
-        private static readonly string _directoryForSettingTheSpritePosition = $"{DirectoryForTheSettings}\\Sprites\\Position.txt";
-
-        private static readonly string _directoryForTheConfigurationOfThePositionOfScale = $"{DirectoryForTheSettings}\\Text-Element\\Scale.txt";
-        private static readonly string _directoryForTheConfigurationOfThePositionOfTheTextElement = $"{DirectoryForTheSettings}\\Text-Element\\Position.txt";
+        private static readonly string _directoryForTheConfigurationOfTheTextElement = $"\\Text-Elements\\Text-Element-Parameter.ini";
 
         public TextManager()
         {
-
+            CreateTheParameterFiles();
         }
 
         void SaveConfigOfPosition()
@@ -34,6 +33,30 @@ namespace Speedometer.Text_Manager
         {
             using (var FileContent = new StreamReader(path))
                 return FileContent.ReadToEnd();
+        }
+
+        void CreateTheParameterFiles()
+        {
+            var directoryToSettings = GetRelativeFilePath("Speedometer\\Settings");
+            var styleGarp = "------------";
+
+            var spriteParameterFile = ScriptSettings.Load($"{directoryToSettings}\\{_directoryForSettingTheSprite}");
+                spriteParameterFile.SetValue("speedometer", $"{styleGarp}@Resolution", GTA.UI.Screen.Resolution);
+                spriteParameterFile.SetValue("speedometer", $"{styleGarp}@AspectRatio", GTA.UI.Screen.AspectRatio);
+                spriteParameterFile.SetValue("speedometer", "position", SpeedometerSettings.Position);
+                spriteParameterFile.SetValue("speedometer", "size", SpeedometerSettings.Size);
+            spriteParameterFile.Save();
+            
+            var textElementParameterFile = ScriptSettings.Load($"{directoryToSettings}\\{_directoryForTheConfigurationOfTheTextElement}");
+                textElementParameterFile.SetValue("Distance", "position", 0f);
+                textElementParameterFile.SetValue("Distance", "scale", 0f);
+
+                textElementParameterFile.SetValue("Speed", "position", 0f);
+                textElementParameterFile.SetValue("Speed", "scale", 0f);
+
+                textElementParameterFile.SetValue("Time", "position", 0f);
+                textElementParameterFile.SetValue("Time", "scale", 0f);
+             textElementParameterFile.Save();
         }
     }
 }
