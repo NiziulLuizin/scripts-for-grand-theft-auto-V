51df960187d10efb0abb583a561147604ffbf510
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-25 21:17:12 -0300

    adding and correcting some things in the class that handles icons

--- Speedometer/Directory-Manager/Content/SpeedometerIconsFolder.cs
+++ Speedometer/Directory-Manager/Content/SpeedometerIconsFolder.cs
@@ -1,39 +1,66 @@
-﻿namespace Speedometer.Directory_Manager.Content
+﻿using GTA;
+
+namespace Speedometer.Directory_Manager.Content
 {
     sealed class SpeedometerIconsFolder : DirectoryManager
     {
-        private static readonly string[] _pathIcons = 
+        private static readonly string[] _pathIcons =
         {
-            $"{PathOfIcons}Metric\\",
-            $"{PathOfIcons}Imperial\\"
+            $@"{PathOfIcons}\",
+            $@"{PathOfIcons}Metric\",
+            $@"{PathOfIcons}Imperial\"
         };
 
-        private static string[] IconsMetric 
+        internal static string[] SimpleIcons
+        { get { return ReturnAllSimpleIcons(); } }
+        internal static string[] MetricIcons
         { get { return ReturnAllMetricIcons(); } }
-        private static string[] IconsImperial 
+        internal static string[] ImperialIcons
         { get { return ReturnAllImperialIcons(); } }
 
-        internal static string GiveMeThePathOfThis(int icon, GTA.MeasurementSystem type)
+        internal static string GiveMeThePathOfThis(int icon, string type)
         {
-            if (type is GTA.MeasurementSystem.Metric)
-                return IconsMetric[icon];
-            else
-                return IconsImperial[icon];
+            return CheckTheTypeAndPassMeTheCorrectIcon(icon, type);
         }
-        internal static string GiveMeTheNameOfThis(int icon, GTA.MeasurementSystem type)
+        internal static string GiveMeTheNameOfThis(int icon, string type)
         {
-            if (type is GTA.MeasurementSystem.Metric)
-                return IconsMetric[icon].Remove(0, _pathIcons[0].Length);
-            else
-                return IconsImperial[icon].Remove(0, _pathIcons[1].Length);
+            int path;
+            switch (type)
+            {
+                case "Metric": path = 1;
+                    break;
+                case "Imperial": path = 2;
+                    break;
+                default: path = 0;
+                    break;
+            }
+            return GiveMeThePathOfThis(icon, type).Remove(0, _pathIcons[path].Length);
         }
-        private static string[] ReturnAllMetricIcons()
+
+        private static string CheckTheTypeAndPassMeTheCorrectIcon(int icon, string type)
+        {
+            return type
+                   is "Simple"
+                   ? SimpleIcons[icon]
+                   : type 
+                   is "Metric"
+                   ? MetricIcons[icon]
+                   : type 
+                   is "Imperial"
+                   ? ImperialIcons[icon]
+                   : null;
+        }
+        private static string[] ReturnAllSimpleIcons()
         {
             return ReturnThePathOfTheItemsPresentInThe(_pathIcons[0]);
         }
-        private static string[] ReturnAllImperialIcons()
+        private static string[] ReturnAllMetricIcons()
         {
             return ReturnThePathOfTheItemsPresentInThe(_pathIcons[1]);
+        }
+        private static string[] ReturnAllImperialIcons()
+        {
+            return ReturnThePathOfTheItemsPresentInThe(_pathIcons[2]);
         }
     }
 }
