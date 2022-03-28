0f18123289ef39c06c4c6fc054e91646509d406b
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-27 22:47:26 -0300

    I created this static class to be responsible for maintaining the directory links

---
+++ Speedometer/Directory-Manager/Settings/DirectorySetup.cs
@@ -0,0 +1,19 @@
+﻿using Speedometer.Editor_Mode;
+
+namespace Speedometer.Directory_Manager.Settings
+{
+    abstract class DirectorySetup
+    {
+        private static string Directory 
+        { 
+            get { return EditorMode.PathToTheScriptFolder;} 
+        }
+
+        protected static string PathToUserInterface
+        { get { return $@"{Directory}UI\"; } }
+        protected static string PathOfIcons
+        { get { return $@"{PathToUserInterface}SpeedometerIcons\"; } }
+        protected static string PathOfBaseToSpeedometer
+        { get { return $@"{PathToUserInterface}BaseForSpeedometer\"; } }
+    }
+}
