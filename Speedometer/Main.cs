28ba383b02093db8788d43bd3e932a470ba1ab0e
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-27 23:38:01 -0300

    The current state of the main class

--- Speedometer/Main.cs
+++ Speedometer/Main.cs
@@ -1,29 +1,47 @@
 ﻿using GTA;
 using Speedometer.Draw.Sprites.CurrentSpeedometer;
-using Speedometer.Editor_Mode;
+using System.Drawing;
 
 namespace Speedometer
 {
     public class Main : Script
     {
-        public bool IsToActivateTheNewSprite { get; private set; }
         public Main()
-        {     
+        {
+            ///
+            ///string pathOfIcon;
+            ///
+            ///SizeF currentSizeOfIcons;
+
+            string[] type =
+            {
+                "Simple",
+                "Metric",
+                "Imperial"
+            };
+
+            ///using (var _ = new SpeedometerIconsFolder())
+            ///pathOfIcon = _.GiveMeThePathOfThis(icon: 3, type[1]);
+
+            ///using (var _ = Bitmap.FromFile(pathOfIcon))
+            ///currentSizeOfIcons = _.Size;
+
+            ///var correntPointF = 
+            ///new PointF(840f, 650f);
+
+            ///var iconsSprites =
+            ///new CustomSprite(pathOfIcon, new SizeF(currentSizeOfIcons.Width / 6, currentSizeOfIcons.Height / 6), correntPointF) { Centered = true };
+
+            var _offset =
+                new SizeF(1f, 1f);
+
+            var SpeedometerBase =
+                    new CurrentSpeedometer().GettingThe(baseSpeedometer: 3);
+
             Tick += (o, e) =>
             {
-                if (Game.Player.Character.IsSittingInVehicle() && !EditorMode.IsEditorModeEnabled)
-                {
-                    CurrentSpeedometer.Draw();
-                    //SpriteManager.DisplayTheCurrentSpeedometer();
-                }
-                
-                if (IsToActivateTheNewSprite) { }
-                                 
-            };
-            KeyUp += (o, e) =>
-            {
-                if (e.KeyCode == System.Windows.Forms.Keys.CapsLock)
-                    IsToActivateTheNewSprite = !IsToActivateTheNewSprite;
+                if (!Game.IsLoading)
+                    SpeedometerBase.ScaledDraw(_offset);
             };
         }
     }
