3c3eabf2df0a1d48cdb7fb1095e16d889c22bbe7
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-03-27 22:45:38 -0300

    Create the class responsible for directory management

---
+++ Speedometer/Directory-Manager/DirectoryManager.cs
@@ -0,0 +1,24 @@
+﻿using Speedometer.Directory_Manager.Settings;
+
+namespace Speedometer.Directory_Manager
+{
+    abstract class DirectoryManager : DirectorySetup, System.IDisposable
+    {
+        protected static string[] ReturnThePathOfTheItemsPresentInThe(string folder)
+        {
+            return System.IO.Directory.GetFiles(folder, "*.png");
+        }
+
+        protected virtual string GiveMeThePathOfThis(ushort item)
+        { throw new System.NotImplementedException(); }
+        protected virtual string GiveMeThePathOfThis(ushort item, string type)
+        { throw new System.NotImplementedException(); }
+        protected virtual string GiveMeTheNameOfThis(ushort baseSpeedometer)
+        { throw new System.NotImplementedException(); }
+        protected virtual string GiveMeTheNameOfThis(ushort baseSpeedometer, string type)
+        { throw new System.NotImplementedException(); }
+
+        public virtual void Dispose()
+        { throw new System.NotImplementedException(); }
+    }
+}
