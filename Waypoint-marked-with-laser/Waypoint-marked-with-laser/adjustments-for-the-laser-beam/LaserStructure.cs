a86d48ccf46db03a7583dc3ab0508a0528611278
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-05-31 03:04:14 -0300

    Waypoint Marked With Laser | v0.2.0.0

---
+++ Waypoint-marked-with-laser/Waypoint-marked-with-laser/adjustments-for-the-laser-beam/LaserStructure.cs
@@ -0,0 +1,89 @@
+﻿using GTA;
+using GTA.Math;
+
+namespace Waypoint_marked_with_laser.adjustments_for_the_laser_beam
+{
+    class LaserStructure
+    {
+        private Vector3 _position =
+            new Vector3();
+
+        private Prop[] _laserStructures =
+            new Prop[2];
+
+        private readonly string[] _modelNameLaser =
+        {
+            "xs_prop_arena_barrel_01a_sf",
+            "xs_prop_arena_bollard_side_01a_sf"
+        };
+
+
+
+        public LaserStructure() { }
+
+        internal void CreateTheLaserStructure()
+        {
+            _position =
+                World.WaypointPosition;
+
+            var hasPhysics =
+                true;
+
+            var IsToStayOnTheGround =
+                false;
+
+            var laserHeight =
+                new Vector3(_position.X,
+                            _position.Y,
+                            _position.Z + 2000f);
+
+
+            _laserStructures[0] =
+                World
+                    .CreateProp(_modelNameLaser[1],
+                                laserHeight,
+                                hasPhysics,
+                                IsToStayOnTheGround);
+
+            _laserStructures[1] =
+                World
+                    .CreateProp(_modelNameLaser[0],
+                                laserHeight,
+                                hasPhysics,
+                                IsToStayOnTheGround);
+
+
+            BasicLaserStructureSettings();
+        }
+        private void BasicLaserStructureSettings()
+        {
+            var rotation =
+                new Vector3(x: 0.20512519f,
+                            y: 179.459076f,
+                            z: 101.254494f);
+
+            var relativeRotation =
+                new Vector3(x: 0f,
+                            y: 2.49999976f,
+                            z: -0.399999976f);
+
+
+            _laserStructures[1].Rotation =
+                rotation;
+
+            _laserStructures[0]
+                .AttachTo(_laserStructures[1],
+                          relativeRotation);
+        }
+        internal void DestroyTheLaserStructure()
+        {
+            foreach (var laserStructure in _laserStructures)
+            {
+                if (laserStructure != null)
+                {
+                    laserStructure.Delete();
+                }
+            }
+        }
+    }
+}
--- Waypoint-marked-with-laser/Waypoint-marked-with-laser/setup-manager/SettingColor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Drawing;
-
-namespace Waypoint_marked_with_laser.setup_manager
-{
-    struct SettingColor
-    {
-        internal static Color ColorOfTheLaserBeam { get; set; }
-    }
-}
