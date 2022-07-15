b5386dcf39d52f06757ce0bc6614b346bc0240e5
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2022-07-15 00:13:31 -0300

    [_0.1.0.0_] ~ Dynamic Hiding Of Minimap- Commit - 1

---
+++ Dynamic-hiding-of-minimap/Dynamic-hiding-of-minimap/Main.cs
@@ -0,0 +1,61 @@
+﻿using GTA;
+using GTA.UI;
+
+
+namespace Dynamic_hiding_of_minimap
+{
+    internal sealed class Main : Script
+    {
+        public Main()
+        {
+            Interval =
+                3500;
+
+            Tick += (o, e) =>
+            { Start(); };
+        }
+
+        private void Start()
+        {
+            var character =
+                Game
+                    .Player
+                        .Character;
+
+            var isInVehicle =
+                    character
+                        .IsSittingInVehicle();
+
+
+            switch (isInVehicle)
+            {
+                case true:
+                    {
+                        var minimapVisibility =
+                            Hud
+                                .IsRadarVisible;
+
+                        if (minimapVisibility)
+                            return;
+                        else
+                            Hud
+                                .IsRadarVisible = true;
+                    }
+                    break;
+                case false:
+                    {
+                        var minimapVisibility =
+                            Hud
+                                .IsRadarVisible;
+
+                        if (minimapVisibility)
+                            return;
+                        else
+                            Hud
+                                .IsRadarVisible = false;
+                    }
+                    break;
+            }
+        }
+    }
+}
